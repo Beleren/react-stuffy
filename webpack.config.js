949ebc239e8d40b68d119a4ef8e0250bf8d028b6
Leandro Haruki <leandroharuki@gmail.com>
2019-07-02 21:27:00 -0300

    Adding webpack babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),

849915e8c0d0e50b37f225adbdfa106cbf094fb0
Leandro Haruki Futema <leandroharuki@gmail.com>
2019-10-26 12:40:59 -0300

    Adding helmet and new font-family

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import './App.scss';
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Helmet>
+        <title>Who am i?</title>
+        <meta charset="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,500,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+
       <h1> Stuffyy </h1>
-    </div>
+    </>
   );
 };
 

4732037f6ea79c2f92c416d0fc8aa26febdccff8
Leandro Haruki Futema <leandroharuki@gmail.com>
2019-10-27 19:20:29 -0300

    Adding initial navigation

--- src/scenes/Home/Home-view.jsx
+++ src/scenes/Home/Home-view.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Home.scss';
+import About from './components/About';
+import Skills from './components/Skills';
+import Service from './components/Service';
 
 const Home = () => {
-  return <h1>Who am i?</h1>;
+  const [currentSection, setCurrentSection] = useState(About);
+  return (
+    <div className="home">
+      <div className="home__content">{currentSection}</div>
+      <nav className="home__nav">
+        <ul>
+          <li onClick={() => setCurrentSection(About)}>Who am i?</li>
+          <li onClick={() => setCurrentSection(Skills)}>What can i do?</li>
+          <li onClick={() => setCurrentSection(Service)}>
+            How can i help you?
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;

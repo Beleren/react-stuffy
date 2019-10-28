c9ce1afcc4e9dc78608c027edf363c752ec071be
Leandro Haruki Futema <leandroharuki@gmail.com>
2019-10-27 21:30:22 -0300

    Removing babel preset from webpack

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
       },
       {
         test: /\.scss$/,
